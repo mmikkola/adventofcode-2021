10002588c52b9c46d6438a66dfb5d0b09cfc6b61
Mattias Mikkola <mattias@mikkola.me>
2021-12-05 14:08:29 +0100

    Day 5

---
+++ adventofcode-2021.tests/Day5Tests.cs
@@ -0,0 +1,41 @@
+﻿using adventofcode_2021.Problems;
+using Xunit;
+using FluentAssertions;
+
+namespace adventofcode_2021.tests
+{
+    public class Day5Tests
+    {
+        private readonly string[] input = new string[]
+        {
+            "0,9 -> 5,9",
+            "8,0 -> 0,8",
+            "9,4 -> 3,4",
+            "2,2 -> 2,1",
+            "7,0 -> 7,4",
+            "6,4 -> 2,0",
+            "0,9 -> 2,9",
+            "3,4 -> 1,4",
+            "0,0 -> 8,8",
+            "5,5 -> 8,2"
+        };
+
+        [Fact]
+        public void Part1Test()
+        {
+            var day5 = new Day5();
+            var result = day5.Part1(input);
+
+            result.Should().Be(5);
+        }
+
+        [Fact]
+        public void Part2Test()
+        {
+            var day5 = new Day5();
+            var result = day5.Part2(input);
+
+            result.Should().Be(12);
+        }
+    }
+}
---
+++ adventofcode-2021/Problems/Day5.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode_2021.Problems
+{
+    public class Day5
+    {
+        private struct Point
+        {
+            public int X;
+            public int Y;
+        }
+
+        private Point Sub(Point a, Point b)
+        {
+            return new Point() { X = a.X - b.X, Y = a.Y - b.Y };
+        }
+
+        private IEnumerable<Point> Parse(string input, bool onlyStraightVents)
+        {
+            var points = new List<Point>();
+            var coords = input.Split(" -> ").Select(p => new Point() { X = int.Parse(p.Split(',')[0]), Y = int.Parse(p.Split(',')[1]) });
+
+            Point pointA = coords.First();
+            Point pointB = coords.Last();
+
+
+            if (onlyStraightVents && (pointA.X != pointB.X && pointA.Y != pointB.Y))
+                return points;
+
+            int startX = Math.Min(pointA.X, pointB.X);
+
+            Point travel = Sub(pointA, pointB);
+
+            points.Add(pointB);
+
+            Point location = new Point() { X = pointB.X, Y = pointB.Y };
+
+            while(travel.X != 0 || travel.Y != 0)
+            {
+                if(travel.X > 0)
+                {
+                    location.X++;
+                    travel.X--;
+                }
+                if(travel.Y > 0)
+                {
+                    location.Y++;
+                    travel.Y--;
+                }
+                if(travel.X < 0)
+                {
+                    location.X--;
+                    travel.X++;
+                }
+                if(travel.Y < 0)
+                {
+                    location.Y--;
+                    travel.Y++;
+                }
+
+                points.Add(new Point() { X = location.X, Y = location.Y });
+            }
+
+            return points;
+        }
+        private int FindOverlaps(string[] input, bool onlyStraightVents)
+        {
+            var overlaps = new Dictionary<Point, int>();
+
+            foreach (string vent in input)
+            {
+                var points = Parse(vent, onlyStraightVents);
+                foreach (var point in points)
+                {
+                    if (overlaps.ContainsKey(point))
+                    {
+                        overlaps[point]++;
+                    }
+                    else
+                    {
+                        overlaps[point] = 1;
+                    }
+                }
+            }
+
+            return overlaps.Count(kvp => kvp.Value > 1);
+        }
+
+        public int Part1(string[] input)
+        {
+            return FindOverlaps(input, true);
+        }
+
+
+        public int Part2(string[] input)
+        {
+            return FindOverlaps(input, false);
+        }
+    }
+}
