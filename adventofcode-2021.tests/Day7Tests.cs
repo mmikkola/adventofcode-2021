7296adf67a3623512a9f662a5e2a4aa90cfa4ed0
Mattias Mikkola <mattias@mikkola.me>
2021-12-07 20:30:09 +0100

    Day 7

---
+++ adventofcode-2021.tests/Day7Tests.cs
@@ -0,0 +1,32 @@
+﻿using adventofcode_2021.Problems;
+using Xunit;
+using FluentAssertions;
+
+namespace adventofcode_2021.tests
+{
+    public class Day7Tests
+    {
+        private readonly string[] input = new string[]
+        {
+            "16,1,2,0,4,2,7,1,2,14"
+        };
+
+        [Fact]
+        public void Part1Test()
+        {
+            var day = new Day7();
+            var result = day.Part1(input);
+
+            result.Should().Be(37);
+        }
+
+        [Fact]
+        public void Part2Test()
+        {
+            var day = new Day7();
+            var result = day.Part2(input);
+
+            result.Should().Be(168);
+        }
+    }
+}
---
+++ adventofcode-2021/Problems/Day7.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode_2021.Problems
+{
+    public class Day7
+    {
+        public int Part1(string[] args)
+        {
+            int[] crabs = args[0].Split(',').Select(int.Parse).ToArray();
+
+            int[] distanceCosts = new int[crabs.Max()];
+
+            for(int i = 0; i < crabs.Max(); i++)
+            {
+                foreach(int crab in crabs)
+                {
+                    distanceCosts[i] += Math.Abs(crab - i); 
+                }
+            }
+
+            return distanceCosts.Min();
+        }
+
+        public int Part2(string[] args)
+        {
+            int[] crabs = args[0].Split(',').Select(int.Parse).ToArray();
+
+            int[] distanceCosts = new int[crabs.Max()];
+
+            for (int i = 0; i < crabs.Max(); i++)
+            {
+                foreach (int crab in crabs)
+                {
+                    distanceCosts[i] += CalculateFuelCost(crab, i);
+                }
+            }
+
+            return distanceCosts.Min();
+        }
+
+        private int CalculateFuelCost(int from, int to)
+        {
+            int distance = Math.Abs(from - to);
+
+            int cost = 0;
+
+            for(int i = 1; i <= distance; i++)
+            {
+                cost += i;
+            }
+
+            return cost;
+        }
+    }
+}
