d57a62c37b817270975ba96167aa04cc3ada69f5
Mattias Mikkola <mattias.mikkola@shoppa.com>
2021-12-10 08:42:08 +0100

    Clean up Program.cs a bit

--- adventofcode-2021/Program.cs
+++ adventofcode-2021/Program.cs
@@ -2,7 +2,19 @@
 using adventofcode_2021.Problems;
 
 var input = File.ReadAllLines("Inputs/day9.txt");
-var result = (new Day9()).Part2(input);
-
-Console.WriteLine(result);
-Console.ReadLine();
+var day = new Day9();
+var timer = System.Diagnostics.Stopwatch.StartNew();
+try
+{
+    var part1 = day.Part1(input);
+    long elapsed = timer.ElapsedMilliseconds;
+    Console.WriteLine($"Solved Part 1 in {elapsed}ms. Result: {part1}");
+} catch { }
+timer.Restart();
+try
+{
+    var part1 = day.Part2(input);
+    long elapsed = timer.ElapsedMilliseconds;
+    Console.WriteLine($"Solved Part 2 in {elapsed}ms. Result: {part2}");
+}
+catch { }
