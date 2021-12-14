c222386bef7b9c1272b35d7e1f75336a16d9e389
Mattias Mikkola <mattias.mikkola@shoppa.com>
2021-12-14 15:30:09 +0100

    Caching helped

--- adventofcode-2021/Problems/Day14.cs
+++ adventofcode-2021/Problems/Day14.cs
@@ -7,71 +7,96 @@ using System.Threading.Tasks;
 namespace adventofcode_2021.Problems
 {
     public class Day14 : ProblemBase
-    {                
-        public override object Part1(string[] input)
+    {
+        private class Polymerizer
         {
-            return Polymerize(input, 10);
-        }
+            private IDictionary<string, string> insertionRules;
+            private string template;
+            private Dictionary<(string, int), IDictionary<char, long>> sequenceCache;
 
-        private long Polymerize(string[] input, int steps)
-        {
-            string template = input.First();
-
-            IDictionary<string, string> insertionRules =
-                input.Skip(2)
-                .Select(x => x.Split(" -> "))
-                .ToDictionary(x => x[0], x => x[1]);
-
-            IDictionary<char, long> occurances = FindElements(template, insertionRules, 0, steps - 1);
-
-            return occurances.Max(kvp => kvp.Value) - occurances.Min(kvp => kvp.Value);
-        }
-
-        private IDictionary<char, long> FindElements(string template, IDictionary<string, string> insertionRules, int currentDepth, int maxDepth)
-        {           
-            var occurances = new Dictionary<char, long>();
-
-            if (currentDepth > maxDepth)
+            public Polymerizer(string[] input)
             {
-                foreach(char c in template)
+                sequenceCache = new Dictionary<(string, int), IDictionary<char, long>>();
+
+                insertionRules =
+                    input.Skip(2)
+                    .Select(x => x.Split(" -> "))
+                    .ToDictionary(x => x[0], x => x[1]);
+
+                template = input.First();
+            }
+
+            public IDictionary<char, long> FindElements(int maxDepth)
+            {
+                return FindElements(template, 0, maxDepth - 1);
+            }
+
+            private IDictionary<char, long> FindElements(string template, int currentDepth, int maxDepth)
+            {           
+                var occurances = new Dictionary<char, long>();
+
+                if (currentDepth > maxDepth)
                 {
-                    if(occurances.ContainsKey(c))
-                        occurances[c]++;
-                    else
-                        occurances[c] = 1;
+                    foreach(char c in template)
+                    {
+                        if(occurances.ContainsKey(c))
+                            occurances[c]++;
+                        else
+                            occurances[c] = 1;
+                    }
+
+                    return occurances;
+                }
+
+                for (int i = 1; i < template.Length; i++)
+                {
+                    string element = template.Substring(i - 1, 2);
+
+                    string toInsert = insertionRules[element];
+
+                    string newElement = $"{element[0]}{toInsert}{element[1]}";
+
+                    IDictionary<char, long> subOccurances;
+
+                    if(!sequenceCache.TryGetValue((newElement, currentDepth), out subOccurances))
+                    {
+                        subOccurances = FindElements(newElement, currentDepth + 1, maxDepth);
+                        sequenceCache[(newElement, currentDepth)] = subOccurances;
+                    }
+
+                    foreach (var kvp in subOccurances)
+                    {
+                        if (occurances.ContainsKey(kvp.Key))
+                            occurances[kvp.Key] += kvp.Value;
+                        else
+                            occurances[kvp.Key] = kvp.Value;
+                    }
+
+                    if (i < template.Length - 1)
+                        occurances[element[1]]--;
                 }
 
                 return occurances;
             }
+        }
 
-            for (int i = 1; i < template.Length; i++)
-            {
-                string element = template.Substring(i - 1, 2);
 
-                string toInsert = insertionRules[element];
+        public override object Part1(string[] input)
+        {
+            var polymerizer = new Polymerizer(input);
 
-                string newElement = $"{element[0]}{toInsert}{element[1]}";
+            var occurances = polymerizer.FindElements(10);
 
-                var subOccurances = FindElements(newElement, insertionRules, currentDepth + 1, maxDepth);
-
-                foreach (var kvp in subOccurances)
-                {
-                    if (occurances.ContainsKey(kvp.Key))
-                        occurances[kvp.Key] += kvp.Value;
-                    else
-                        occurances[kvp.Key] = kvp.Value;
-                }
-
-                if (i < template.Length - 1)
-                    occurances[element[1]]--;
-            }
-
-            return occurances;
+            return occurances.Max(kvp => kvp.Value) - occurances.Min(kvp => kvp.Value);
         }
 
         public override object Part2(string[] input)
         {
-            return Polymerize(input, 40);
+            var polymerizer = new Polymerizer(input);
+
+            var occurances = polymerizer.FindElements(40);
+
+            return occurances.Max(kvp => kvp.Value) - occurances.Min(kvp => kvp.Value);
         }
     }
 }
